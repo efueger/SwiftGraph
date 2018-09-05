23ffe7e604a678403d3a8ef2d23ff499f7b58963
Emily Fueger <efueger@users.noreply.github.com>
2018-09-05 17:01:00 -0400

    Update Graph.swift

--- Sources/SwiftGraph/Graph.swift
+++ Sources/SwiftGraph/Graph.swift
@@ -287,4 +287,23 @@ open class Graph<V: Equatable>: CustomStringConvertible, Sequence, Collection {
     public subscript(i: Int) -> V {
         return vertexAtIndex(i)
     }
+    
+    
+        public func neighborsForVertex(_ vertex: V) -> [V]? {
+        if let i = indexOfVertex(vertex) {
+            return neighborsForIndex(i)
+        }
+        return nil
+    }
+    
+    /// Find all of the edges of a vertex at a given index.
+    ///
+    /// - parameter index: The index for the vertex to find the children of.
+    public func edgesForIndex(_ index: Int) -> [Edge] {
+        return edges[index]
+    }
+    
+    /// Find all of the edges of a given vertex.
+    ///
+    /// - parameter vertex: The vertex to find the edges of.
 }
